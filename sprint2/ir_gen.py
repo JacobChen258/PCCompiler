77b36d153eb0e42b0cf59221c012aaed0e960686
Yifei Yin <yifeiyin@foxmail.com>
2022-03-01 17:59:48 -0500

    Add ir_gen

---
+++ sprint2/ir_gen.py
@@ -0,0 +1,198 @@
+import AST
+
+"""
+- goto: label
+- op: left, right, operator, result
+- push-param: var
+- pop-param: var
+- if: var, label for false, label for end
+- elif: TODO
+- else: TODO
+- while: var, label for start, label for end
+
+- list: ???
+- for-loop: ???
+- string: ???
+
+- function declaration
+  - return statement
+- function call
+
+"""
+
+class IRGen:
+    def __init__(self):
+        self.IR = []
+        self.register_count = 0
+        self.label_count = 0
+
+    def generate(self, node):
+        method = 'gen_' + node.__class__.__name__
+        return getattr(self, method)(node)
+
+    def add_code(self, code):
+        self.IR.append(code)
+
+    def inc_register(self):
+        self.register_count += 1
+        return self.register_count
+
+    def reset_register(self):
+        self.register_count = 0
+
+    def inc_label(self):
+        self.label_count += 1
+        return self.label_count
+
+    def mark_label(self, label):
+        self.IR_lst.append("_L{}:".format(label))
+
+    def print_ir(self):
+        for ir in self.IR_lst:
+            print(ir)
+
+    ###################################
+
+
+    def gen_FunctionDef(self, node: AST.FunctionDef):
+        pass
+
+    def gen_FunctionCall(self, node: AST.FunctionCall):
+        pass
+
+    def gen_ReturnStmt(self, node: AST.ReturnStmt):
+        pass
+
+    def gen_Assignment(self, node: AST.Assignment):
+        pass
+
+    def gen_RangeValues(self, node: AST.RangeValues):
+        pass
+
+    def gen_ForLoopList(self, node: AST.ForLoopList):
+        pass
+
+    def gen_ForLoopRange(self, node: AST.ForLoopRange):
+        pass
+
+    def gen_PrimitiveLiteral(self, node: AST.PrimitiveLiteral):
+        pass
+
+    def gen_NonPrimitiveLiteral(self, node: AST.NonPrimitiveLiteral):
+        pass
+
+    def gen_BinaryOperation(self, node: AST.BinaryOperation):
+        pass
+
+    def gen_UnaryOperation(self, node: AST.UnaryOperation):
+        pass
+
+    def gen_IfStmt(self, node: AST.IfStmt):
+        pass
+
+    def gen_ElifStmt(self, node: AST.ElifStmt):
+        pass
+
+    def gen_ElseStmt(self, node: AST.ElseStmt):
+        pass
+
+    def gen_WhileStmt(self, node: AST.WhileStmt):
+        pass
+
+    def gen_Id(self, node: AST.Id):
+        pass
+
+"""
+    def gen_AssignStmt(self, node):
+        expr = self.generate(node.expr)
+        self.add_code("{} := {}".format(node.name, expr))
+        self.register_count = 0
+
+    def gen_BinOp(self, node):
+        # Left operand
+        left = self.generate(node.left)
+        # Right operand
+        right = self.generate(node.right)
+
+        reg = self.inc_register()
+        self.add_code("{} := {} {} {}".format('_t%d' % reg, left, node.op, right))
+
+        return '_t%d' % reg
+
+    def gen_Constant(self, node):
+        return node.value
+
+    def gen_DeclStmt(self, node):
+        expr = self.generate(node.expr)
+        self.add_code("{} := {}".format(node.name, expr))
+        self.register_count = 0
+
+    def gen_FuncCall(self, node):
+
+        # Push all of the arguments with "PushParam" function
+        args = node.children()
+        for (i, arg) in args:
+            self.add_code("PushParam %s" % self.generate(arg))
+
+        # Once all of the parameter has been pushed, actually call the function
+        self.add_code("FuncCall %s" % node.name)
+
+        # After we're done with the function, remove the spaces reserved
+        # for the arguments
+        self.add_code("PopParams %d" % len(args))
+
+        reg = self.inc_register()
+        self.add_code("{} := ret".format('_t%d' % reg))
+
+        return '_t%d' % reg
+
+    def gen_IfStmt(self, node):
+        cond = self.generate(node.cond)
+
+        fbranch_label = self.inc_label()
+        tbranch_label = self.inc_label()
+
+        # Skip to the false_body if the condition is not met
+        self.add_code("if !({}) goto {}".format(cond, '_L%d' % fbranch_label))
+        self.generate(node.true_body)
+        # Make sure the statements from false_body is skipped
+        self.add_code("goto _L%d" % tbranch_label)
+
+        self.mark_label(fbranch_label)
+        self.generate(node.false_body)
+        self.mark_label(tbranch_label)
+
+    def gen_MethodDecl(self, node):
+
+        skip_decl = self.inc_label()
+
+        # We want to skip the function code until it is called
+        self.add_code("goto _L%d" % skip_decl)
+
+        # Function label
+        self.mark_label(node.name)
+
+        # Allocate room for function local variables
+        self.add_code("BeginFunc")
+
+        # Actually generate the main body
+        self.generate(node.body)
+        self.generate(node.ret_stmt)
+
+        # Do any cleanup before jumping back
+        self.add_code("EndFunc")
+
+        self.mark_label(skip_decl)
+
+    def gen_Program(self, node):
+        for (child_name, child) in node.children():
+            self.generate(child)
+
+    def gen_RetStmt(self, node):
+        expr = self.generate(node.expr)
+        self.add_code("ret := {}".format(expr))
+
+    def gen_StmtList(self, node):
+        for stmt in node.stmt_lst:
+            self.generate(stmt)
+"""
