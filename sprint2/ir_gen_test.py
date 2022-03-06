87bca2b405f538751bd959652b2276ac74ac44d4
Yifei Yin <yifeiyin@foxmail.com>
2022-03-06 11:23:31 -0500

    Fix yacc test 20

---
+++ sprint2/ir_gen_test.py
@@ -0,0 +1,60 @@
+import pytest
+import os
+import difflib
+from yacc import pythonParser
+from type_checker import TypeChecker, SymbolTable
+from ir_gen import IRGen
+
+@pytest.fixture
+def parser():
+    p = pythonParser()
+    p.build()
+    return p
+
+test_dir = 'ir_gen_tests'
+files = os.listdir(f'./{test_dir}/')
+test_names = [f.replace('_input.py', '') for f in files if f.endswith('_input.py')]
+
+
+def format_parser_output(o):
+    import json
+    return json.dumps(o, default=lambda x: { 'NODE': x.__class__.__name__, **x.__dict__}, indent=2)
+
+
+@pytest.mark.parametrize("test_name", test_names)
+def test_simple_token(parser, test_name):
+    with open(f'./{test_dir}/{test_name}_input.py', 'r') as f:
+        input_str = f.read()
+    try:
+        with open(f'./{test_dir}/{test_name}_output.txt', 'r') as f:
+            output_str = f.read().strip()
+    except FileNotFoundError:
+        print(f'No output file for {test_name}')
+        output_str = None
+
+    received = parser.parse(input_str)
+    tc = TypeChecker()
+    tc.typecheck(received[0], SymbolTable())
+    ir_generator = IRGen()
+    ir_generator.generate_IR(received)
+
+    received_str = ""
+    for ir in ir_generator.IR:
+        received_str += repr(ir)  + ' \n'
+
+    with open(f'./{test_dir}/{test_name}_received.txt', 'w+') as f:
+        f.write(received_str)
+
+    if output_str is not None:
+        if received_str.strip().strip("\n") != output_str.strip().strip("\n"):
+            diff = difflib.unified_diff(output_str.split('\n'), received_str.split('\n'), f"./tests/{test_name}_output.txt", f"./tests/{test_name}_received.txt", lineterm='')
+            with open(f'./{test_dir}/{test_name}_received.diff', 'w+') as f:
+                f.write('\n'.join(diff) + '\n')
+            raise AssertionError(f'{test_name} failed')
+        else:
+            try:
+                os.remove(f'./{test_dir}/{test_name}_received.diff')
+            except FileNotFoundError:
+                pass
+    else:
+        raise AssertionError(f'{test_name} missing output file')
