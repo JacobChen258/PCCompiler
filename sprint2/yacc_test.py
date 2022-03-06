049cfd8143e60e27793a38fee9079c770d4b4d63
Yifei Yin <yifeiyin@foxmail.com>
2022-03-06 11:23:23 -0500

    Move yacc_tests

--- sprint2/yacc_test.py
+++ sprint2/yacc_test.py
@@ -10,7 +10,8 @@ def parser():
     p.build()
     return p
 
-files = os.listdir('./tests/')
+test_dir = 'yacc_tests'
+files = os.listdir(f'./{test_dir}/')
 test_names = [f.replace('_input.py', '') for f in files if f.endswith('_input.py')]
 
 
@@ -21,10 +22,10 @@ def format_parser_output(o):
 
 @pytest.mark.parametrize("test_name", test_names)
 def test_simple_token(parser, test_name):
-    with open(f'./tests/{test_name}_input.py', 'r') as f:
+    with open(f'./{test_dir}/{test_name}_input.py', 'r') as f:
         input_str = f.read()
     try:
-        with open(f'./tests/{test_name}_output.json', 'r') as f:
+        with open(f'./{test_dir}/{test_name}_output.json', 'r') as f:
             output_str = f.read().strip()
     except FileNotFoundError:
         print(f'No output file for {test_name}')
@@ -35,18 +36,18 @@ def test_simple_token(parser, test_name):
     for i in received:
         received_str += format_parser_output(i)  + ' \n'
 
-    with open(f'./tests/{test_name}_received.json', 'w+') as f:
+    with open(f'./{test_dir}/{test_name}_received.json', 'w+') as f:
         f.write(received_str)
 
     if output_str is not None:
         if received_str.strip().strip("\n") != output_str.strip().strip("\n"):
             diff = difflib.unified_diff(output_str.split('\n'), received_str.split('\n'), f"./tests/{test_name}_output.json", f"./tests/{test_name}_received.json", lineterm='')
-            with open(f'./tests/{test_name}_received.diff', 'w+') as f:
+            with open(f'./{test_dir}/{test_name}_received.diff', 'w+') as f:
                 f.write('\n'.join(diff) + '\n')
             raise AssertionError(f'{test_name} failed')
         else:
             try:
-                os.remove(f'./tests/{test_name}_received.diff')
+                os.remove(f'./{test_dir}/{test_name}_received.diff')
             except FileNotFoundError:
                 pass
     else:
