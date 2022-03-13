71cd4d62d983113a1821db24041a9c80b4b5b2c9
Litao Chen <55116264+JacobChen258@users.noreply.github.com>
2022-03-12 21:03:56 -0500

    complete if gen

--- sprint3/C_AST_gen.py
+++ sprint3/C_AST_gen.py
@@ -3,36 +3,40 @@ from ir_gen import *
 
 
 class CASTGenerator:
-    seen_labels = []    # Labels have seen
-    waiting_labels = []     # Labels have not seen
-    temp_st = {}   # Keep track of declared variables
+    seen_labels = []  # Labels have seen
+    waiting_labels = []  # Labels have not seen
+    temp_st = {}  # Keep track of declared variables
     result_AST = []
+    end_if_labels = []  # value is a tuple (label name, head of if)
 
-    def generate_AST(self, ir,st=None):
+    def generate_AST(self, ir, st=None):
+        print("*******")
+        print(ir)
+        print("*******")
         self.ir = ir[:]
         while self.ir:
             ir_line = self.ir.pop(0)
             self.result_AST += self.gen(ir_line, st)
         return C_AST.Block(self.result_AST)
 
-    def gen(self, ir_line,st=None):
+    def gen(self, ir_line, st=None):
         method = 'gen_' + ir_line.__class__.__name__
         try:
-            return getattr(self, method)(ir_line,st)
+            return getattr(self, method)(ir_line, st)
         except AttributeError:
             print(f"Trying to process ir {ir_line}")
             raise
 
-    def lookup_temp(self,name):
+    def lookup_temp(self, name):
         if name in self.temp_st:
             return self.temp_st[name]
         return None
 
-    def gen_IR_Label(self, ir_node:IR_Label,st=None):
+    def gen_IR_Label(self, ir_node: IR_Label, st=None):
         if "FOR" in ir_node.value:
             # do something for FOR
             pass
-        elif "WHILE" in ir_node.vlaue:
+        elif "WHILE" in ir_node.value:
             # do something for WHILE
             pass
         elif 'FUNC' in ir_node.value:
@@ -41,15 +45,15 @@ class CASTGenerator:
             pass
         else:
             # other cases
-            pass
+            return ir_node.value
 
-    def gen_IR_Goto(self,ir_node:IR_Goto,st=None):
+    def gen_IR_Goto(self, ir_node: IR_Goto, st=None):
         if ir_node.label in self.seen_labels:
             pass
         # If label not in seen, it means the following code is in a block
         self.waiting_labels.append(ir_node.label)
 
-    def gen_IR_PrimitiveLiteral(self,ir_node:IR_PrimitiveLiteral,st=None):
+    def gen_IR_PrimitiveLiteral(self, ir_node: IR_PrimitiveLiteral, st=None):
         ''' All primitive literal are assigned to register'''
         type_val = type(ir_node.val)
         # String is not represented as primitive literal in IR
@@ -62,10 +66,10 @@ class CASTGenerator:
         # Primitive Literal Reg will not be assigned again
         self.temp_st[ir_node.reg] = type_val
         id_node = C_AST.Id(name=ir_node.reg)
-        decl_node = C_AST.Declaration(id = id_node,type=C_AST.Type(value=type_val))
-        return [decl_node,C_AST.Assignment(id=id_node,val=ir_node.val)]
+        decl_node = C_AST.Declaration(id=id_node, type=C_AST.Type(value=type_val))
+        return [decl_node, C_AST.Assignment(id=id_node, val=ir_node.val)]
 
-    def gen_IR_BinaryOperation(self,ir_node:IR_BinaryOperation,st=None):
+    def gen_IR_BinaryOperation(self, ir_node: IR_BinaryOperation, st=None):
         result_node = C_AST.Id(name=ir_node.result_reg)
         left_node = C_AST.Id(name=ir_node.left_reg)
         right_node = C_AST.Id(name=ir_node.right_reg)
@@ -76,106 +80,175 @@ class CASTGenerator:
         if not self.lookup_temp(ir_node.result_reg):
             # need assignment
             # should we consider string here?
-            if ir_node.operator in ["<", "<=", "=>", ">"]:
+            if ir_node.operator in ["<", "<=", "=>", ">", "=="]:
                 type_t = 'bool_t'
             else:
-                operand_t = [self.lookup_temp(ir_node.left_reg),self.lookup_temp(ir_node.right_reg)]
+                operand_t = [self.lookup_temp(ir_node.left_reg), self.lookup_temp(ir_node.right_reg)]
                 if 'float_t' in operand_t:
                     type_t = 'float_t'
                 else:
                     type_t = 'int_t'
             self.temp_st[ir_node.result_reg] = type_t
-            decl_node = C_AST.Declaration(id=result_node,type=C_AST.Type(value=type_t))
-            return [decl_node,operation_node]
+            decl_node = C_AST.Declaration(id=result_node, type=C_AST.Type(value=type_t))
+            return [decl_node, operation_node]
         return [operation_node]
 
-    def gen_IR_UnaryOperation(self,ir_node:IR_UnaryOperation,st=None):
+    def gen_IR_UnaryOperation(self, ir_node: IR_UnaryOperation, st=None):
         result_node = C_AST.Id(name=ir_node.result_reg)
         operand_node = C_AST.Id(name=ir_node.left_reg)
-        operation_node = C_AST.UnaryOperation(left=result_node,operator=ir_node.operator,operand=operand_node)
+        operation_node = C_AST.UnaryOperation(left=result_node, operator=ir_node.operator, operand=operand_node)
         if not self.lookup_temp(ir_node.result_reg):
             if ir_node.operator == "!":
                 type_t = "bool_t"
             else:
                 type_t = self.lookup_temp(ir_node.operand_reg)
-            decl_node = C_AST.Declaration(id=result_node,type=C_AST.Type(value=type_t))
-            return [decl_node,operation_node]
+            decl_node = C_AST.Declaration(id=result_node, type=C_AST.Type(value=type_t))
+            return [decl_node, operation_node]
         return [operation_node]
 
-    def gen_IR_PushParam(self,ir_node:IR_PushParam,st=None):  # should it be just value rather than register?
+    def gen_IR_PushParam(self, ir_node: IR_PushParam, st=None):  # should it be just value rather than register?
         pass
 
-    def gen_IR_PopParam(self,ir_node:IR_PopParam,st=None):  # number of params to pop?
+    def gen_IR_PopParam(self, ir_node: IR_PopParam, st=None):  # number of params to pop?
         pass
 
-    def gen_IR_FunctionCall(self,ir_node:IR_FunctionCall,st=None):
+    def gen_IR_FunctionCall(self, ir_node: IR_FunctionCall, st=None):
         pass
 
-    def gen_IR_FunctionReturn(self,ir_node:IR_FunctionReturn,st=None):
+    def gen_IR_FunctionReturn(self, ir_node: IR_FunctionReturn, st=None):
         pass
 
-    def gen_IR_ReturnStmt(self,ir_node:IR_ReturnStmt,st=None):
+    def gen_IR_ReturnStmt(self, ir_node: IR_ReturnStmt, st=None):
         pass
 
-    def gen_IR_PushStack(self,ir_node:IR_PushStack,st=None):
+    def gen_IR_PushStack(self, ir_node: IR_PushStack, st=None):
         pass
 
-    def gen_IR_PopStack(self,ir_node:IR_PopStack,st=None):
+    def gen_IR_PopStack(self, ir_node: IR_PopStack, st=None):
         pass
 
-    def gen_IR_IfStmt(self,ir_node:IR_IfStmt,st=None):
-        pass
+    def gen_IR_IfStmt(self, ir_node: IR_IfStmt, st=None):
+        false_label = ir_node.if_false.label
+        idx = 0
+        prev_node = None
+        next_node = self.ir[idx]
+        # if next node is not label or not the false label, then it is in body
+        print("=====================")
+        while not (next_node.__class__.__name__ == 'IR_Label' and next_node.value == false_label):
+            idx += 1
+            prev_node = next_node
+            next_node = self.ir[idx]
+            print(next_node)
+        # based on the if stmt structure in IR, the end of if label must be before false label
+        self.end_if_labels.append([prev_node.label, 0])
+        if_node = C_AST.IfStmt(ifCond=C_AST.Id(name=ir_node.cond_reg), body=C_AST.Block([]))
+        # signal is set to false when reaching the false label
+        continue_sig = True
+        while continue_sig:
+            node = self.ir.pop(0)
+            val = self.gen(node, st)
+            if val and val == false_label:
+                continue_sig = False
+            elif val:
+                if_node.body.lst += val
+        # call elif to check if we have following elif
+        return self._gen_IR_ElifStmt(self.ir.pop(0), [if_node], st)
 
-    def gen_IR_ElifStmt(self,ir_node:IR_ElifStmt,st=None):
-        pass
+    def _gen_IR_ElifStmt(self, ir_node: any, if_stmt=None, st=None):
+        idx = 0
+        prev_node = None
+        next_node = ir_node
+        continue_sig = True
+        while not (next_node.__class__.__name__ == 'IR_ElifStmt' or \
+                   (next_node.__class__.__name__ == 'IR_Label' and next_node.value == self.end_if_labels[-1][0])):
+            # it has to reach one of the condition without reaching the end of ir
+            prev_node = next_node
+            next_node = self.ir[idx]
+            idx += 1
+        if next_node.__class__.__name__ == 'IR_Label':
+            # if stmts is empty, there is not trailing if statements
+            if not prev_node:
+                self.end_if_labels.pop()
+                return if_stmt
+            result_stmt = C_AST.ElseStmt(body=C_AST.Block([]))
+            cur_node = ir_node
+            while continue_sig:
+                val = self.gen(cur_node, st)
+                if val and val == self.end_if_labels[-1][0]:
+                    continue_sig = False
+                elif val:
+                    result_stmt.body.lst += val
+                if continue_sig:
+                    cur_node = self.ir.pop(0)
+        else:
+            # reach elif, stmts are the conditonal expression, need to be inserted before if head
+            cond_ast = []
+            cur_node = self.ir.pop(0)
+            while cur_node.__class__.__name__ != 'IR_ElifStmt':
+                cond_ast += self.gen(cur_node, st)
+                cur_node = self.ir.pop(0)
+            if_stmt = if_stmt[:self.end_if_labels[-1][1]] + cond_ast + if_stmt[self.end_if_labels[-1][1]:]
+            self.end_if_labels[-1][1] += len(cond_ast)
+            result_stmt = C_AST.ElifStmt(elifCond=C_AST.Id(cur_node.cond_reg), body=C_AST.Block([]))
+            false_label = next_node.elif_false.label
+            while continue_sig:
+                node = self.ir.pop(0)
+                val = self.gen(node, st)
+                if val and val == false_label:
+                    continue_sig = False
+                elif val:
+                    result_stmt.body.lst += val
+        if_stmt.append(result_stmt)
+        if result_stmt.__class__.__name__ == "ElseStmt":
+            self.end_if_labels.pop()
+            return if_stmt
+        return self._gen_IR_ElifStmt(self.ir.pop(0), if_stmt, st)
 
-    def gen_IR_Assignment(self,ir_node:IR_Assignment,st=None):
+    def gen_IR_Assignment(self, ir_node: IR_Assignment, st=None):
         id_node = C_AST.Id(name=ir_node.name)
-        stmt_node = C_AST.Assignment(id=id_node,val=ir_node.val)
+        stmt_node = C_AST.Assignment(id=id_node, val=ir_node.val)
         if not self.lookup_temp(name=ir_node.name):
             type_t = self.lookup_temp(name=ir_node.val)
             self.temp_st[ir_node.name] = type_t
-            decl_node = C_AST.Declaration(id=id_node,type=C_AST.Type(type_t))
-            return [decl_node,stmt_node]
+            decl_node = C_AST.Declaration(id=id_node, type=C_AST.Type(type_t))
+            return [decl_node, stmt_node]
         return [stmt_node]
 
-    def gen_IR_List(self,ir_node:IR_List,st=None):
+    def gen_IR_List(self, ir_node: IR_List, st=None):
         pass
 
-    def gen_IR_List_VAL(self,ir_node:IR_List_VAL,st=None):
+    def gen_IR_List_VAL(self, ir_node: IR_List_VAL, st=None):
         pass
 
-    def gen_IR_LoopStart(self,ir_node:IR_LoopStart,st=None):
+    def gen_IR_LoopStart(self, ir_node: IR_LoopStart, st=None):
         pass
 
-    def gen_IR_LoopStop(self,ir_node:IR_LoopStop,st=None):
+    def gen_IR_LoopStop(self, ir_node: IR_LoopStop, st=None):
         pass
 
-    def gen_IR_LoopStep(self,ir_node:IR_LoopStep,st=None):
+    def gen_IR_LoopStep(self, ir_node: IR_LoopStep, st=None):
         pass
 
-    def gen_IR_String(self,ir_node:IR_String,st=None):
+    def gen_IR_String(self, ir_node: IR_String, st=None):
         pass
 
-    def gen_IR_String_char(self,ir_node:IR_String_char,st=None):
+    def gen_IR_String_char(self, ir_node: IR_String_char, st=None):
         pass
 
-    def gen_IR_Parameter(self,ir_node:IR_Parameter,st=None):
+    def gen_IR_Parameter(self, ir_node: IR_Parameter, st=None):
         pass
 
-    def gen_IR_Parameter_VAL(self,ir_node:IR_Parameter_VAL,st=None):
+    def gen_IR_Parameter_VAL(self, ir_node: IR_Parameter_VAL, st=None):
         pass
 
-    def gen_IR_Argument(self,ir_node: IR_Argument,st=None):
+    def gen_IR_Argument(self, ir_node: IR_Argument, st=None):
         pass
 
-    def gen_IR_Argument_VAL(self,ir_node: IR_Argument_VAL,st=None):
+    def gen_IR_Argument_VAL(self, ir_node: IR_Argument_VAL, st=None):
         pass
 
-    def gen_IR_Address(self,ir_node:IR_Address,st=None):
+    def gen_IR_Address(self, ir_node: IR_Address, st=None):
         pass
 
-    def gen_IR_Deref(self,ir_node:IR_Deref,st=None):
+    def gen_IR_Deref(self, ir_node: IR_Deref, st=None):
         pass
-
-
