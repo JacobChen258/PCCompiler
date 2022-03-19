5f65b9e508165fea8a707fcdf2519cd04c5a395b
Yifei Yin <yifeiyin@foxmail.com>
2022-03-19 14:21:17 -0400

    Fix yacc for literal None

--- sprint3/AST.py
+++ sprint3/AST.py
@@ -52,12 +52,12 @@ class Expression(Node):
 
 @dataclass
 class PrimitiveLiteral(Node):
-    name: Union['str', 'int', 'float', 'bool']
+    name: Literal['str', 'int', 'float', 'bool', 'none']
     value: str
 
 @dataclass
 class NonPrimitiveLiteral(Node):
-    name: Union['tuple', 'list']
+    name: Literal['tuple', 'list']
     children: List[Union[any, None]]
 
 @dataclass(unsafe_hash=True)
