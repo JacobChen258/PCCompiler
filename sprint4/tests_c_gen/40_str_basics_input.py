b5df9bd4f28aed9c19577fd399bc671002fec096
Yifei Yin <yifeiyin@foxmail.com>
2022-04-07 01:03:03 -0400

    Update test case 40

--- sprint4/tests_c_gen/40_str_basics_input.py
+++ sprint4/tests_c_gen/40_str_basics_input.py
@@ -1,7 +1,8 @@
 a: str = "hello"
 b: str = "world"
 c: str = a + b
-e: int = input_int()
-f: int = input_int() + 1
+e: str = input_str()
+f: str = input_str() + c
 # d: str = a + ", " + e
-
+print(e)
+print(f)
