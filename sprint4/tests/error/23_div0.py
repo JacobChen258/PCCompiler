4a24c1769630447db6ab805b14a7e93f268ad82a
Yifei Yin <yifeiyin@foxmail.com>
2022-04-11 00:03:57 -0400

    Remove div by 0 test cases

--- sprint4/tests/error/23_div0.py
+++
@@ -1,5 +0,0 @@
-# Division by 0. This only works with [OPT_ON]
-#
-a: int = 1 / 0
-b: int = 1 + 1
-print(a)
--- sprint4/tests/error/24_div0.py
+++
@@ -1,3 +0,0 @@
-# Division by 0. This also works with opt off and it will error in [RUNTIME]
-#
-print(1 / 0)
